5d209e1f1a2c7e8fd55aa71ddef8006bb2dd3dfa
S0IG0 <chibitok.d.s@edu.mirea.ru>
2023-10-15 02:00:37 +0300

    add config for RedisCache

---
+++ src/main/java/com/soigo/count/configs/CacheConfig.java
@@ -0,0 +1,27 @@
+package com.soigo.count.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Value("${redis.cache.minutes}")
+    Integer lifetimeInMinutes;
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(lifetimeInMinutes));
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
+}
